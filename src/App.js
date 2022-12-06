aef90bcf984a1a3b9444feb1cff3e0f15a18817b
김경일 <A02053@hanatour.com>
2022-12-06 15:31:22 +0900

    ㅇㅇㅇ
    ㅇㅇㅇ

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App  = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          김경일 만세!!
+          안녕하세요
         </p>
         <a
           className="App-link"
